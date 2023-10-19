d1c428f0ac17248736b39f9eeee8c59e2a3790e4
Adhindazhr <adhindaaazh@gmail.com>
2023-10-19 09:16:54 +0700

    index-berita

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,11 @@ Route::get('/', [TakunController::class,'index']);
 
 
 Route::get('alumni', [TalumniController::class, 'index']);
-Route::get('tambah', [TalumniController::class, 'create']);
+Route::get('alumni/tambah', [TalumniController::class, 'create']);
+
+
+// Route::get('berita', [TberitaController::class, 'index']);
+// Route::get('berita/tambah', [TberitaController::class, 'create']);
 
 // Route::prefix('dashboard')->middleware('OnlyAdmin')->group(function(){
     
