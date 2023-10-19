d903d8ac6c34d46d7f046c94275ee932da73ee0f
AsaMalika25 <khairulasa9@gmail.com>
2023-10-19 10:12:19 +0700

    menambahkan db berita dan jenis berita

---
+++ app/Http/Controllers/TberitaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tberita;
+use Illuminate\Http\Request;
+
+class TberitaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(tberita $tberita)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(tberita $tberita)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, tberita $tberita)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(tberita $tberita)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_10_19_023753_create_tberitas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tberita', function (Blueprint $table) {
+            $table->integer('id_berita',true);
+            $table->integer('id_alumni',false)->nullable(false);
+            $table->integer('id_superadmin',false)->nullable(false);
+            $table->integer('id_jenis_berita',false)->nullable(false);
+            $table->string('judul_berita')->nullable(false);
+            $table->date('tgl_post')->nullable(false);
+            $table->integer('kode_berita',false)->nullable(false);
+            $table->text('ket_berita')->nullable(false);
+
+            $table->foreign('id_alumni')->references('id_alumni')->on('talumni')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_superadmin')->references('id_superadmin')->on('tsuperadmin')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_jenis_berita')->references('id_jenis_berita')->on('tjenis_berita')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tberitas');
+    }
+};
