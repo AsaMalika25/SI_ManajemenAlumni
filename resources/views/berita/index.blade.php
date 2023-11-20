d97e1f72a54bf47a21fc3777c15e209889991b8f
Adhindazhr <adhindaaazh@gmail.com>
2023-11-20 10:13:12 +0700

    berita-alumnikaprog

---
+++ resources/views/berita/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layout.layouts')
+@section('content')
+<div class="container mt-5 align-item-center" style="background-repeat: no-repeat; background-size: cover; background-position: center;">
+        <div class="header">
+            <h1 style="color: #fff;"><b>Berita</b></h1>
+        </div>
+        
+        <div class="d-flex">
+          <div class="card" style="width: 18rem; margin-right: 10px;">
+            <img src="img/3966983-removebg-preview.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          <div class="card" style="width: 18rem; margin-right: 10px;">
+            <img src="img/3966983-removebg-preview.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          <div class="card" style="width: 18rem; margin-right: 10px;">
+            <img src="img/3966983-removebg-preview.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          <div class="card" style="width: 18rem;">
+            <img src="img/3966983-removebg-preview.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+    </div>
+@endsection
