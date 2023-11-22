8a67b45322870aee0a63bd41e5bba8cb16aa5a23
Adhindazhr <adhindaaazh@gmail.com>
2023-11-22 10:14:09 +0700

    log-activity-finish

--- database/migrations/2023_10_18_071845_create_tkaprogs_table.php
+++ database/migrations/2023_10_18_071845_create_tkaprogs_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -23,6 +24,13 @@ return new class extends Migration
 
             $table->timestamps();
         });
+
+        DB::unprepared('CREATE TRIGGER tambah_kaprog AFTER INSERT ON tkaprog FOR EACH ROW BEGIN INSERT INTO logs(logs) VALUES (concat
+        (\'kaprog \', NEW.nama_kaprog, \' \', \'telah ditambahkan pada \', \' \', NOW())); END;');
+        DB::unprepared('CREATE TRIGGER edit_kaprog AFTER UPDATE ON tkaprog FOR EACH ROW BEGIN INSERT INTO logs(logs) VALUES (concat
+        (\'kaprog \', OLD.nama_kaprog, \' \', \'telah diperbarui pada \', \' \', NOW())); END;');
+        DB::unprepared('CREATE TRIGGER hapus_kaprog AFTER DELETE ON tkaprog FOR EACH ROW BEGIN INSERT INTO logs(logs) VALUES (concat
+        (\'kaprog \', OLD.nama_kaprog, \' \', \'telah dihapus pada \', \' \', NOW())); END;');
     }
 
     /**
--- database/migrations/2023_10_18_071916_create_tkerjas_table.php
+++ database/migrations/2023_10_18_071916_create_tkerjas_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -21,6 +22,12 @@ return new class extends Migration
 
             $table->timestamps();
         });
+        DB::unprepared('CREATE TRIGGER tambah_kerja AFTER INSERT ON tkerja FOR EACH ROW BEGIN INSERT INTO logs(logs) VALUES (concat
+        (\'Data kerja \', NEW.instansi, \' \', \'telah ditambahkan pada \', \' \', NOW())); END;');
+        DB::unprepared('CREATE TRIGGER edit_kerja AFTER UPDATE ON tkerja FOR EACH ROW BEGIN INSERT INTO logs(logs) VALUES (concat
+        (\'Data kerja \', OLD.instansi, \' \', \'telah diperbarui pada \', \' \', NOW())); END;');
+        DB::unprepared('CREATE TRIGGER hapus_kerja AFTER DELETE ON tkerja FOR EACH ROW BEGIN INSERT INTO logs(logs) VALUES (concat
+        (\'Data kerja \', OLD.instansi, \' \', \'telah dihapus pada \', \' \', NOW())); END;');
     }
 
     /**
